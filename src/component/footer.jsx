db593abb679a7dc94ebaa4c8d9fb39a4440a4331
Anbuvel-2003 <anbusmartanbuvel2407@gmail.com>
2025-06-03 16:47:22 +0530

    first commit

---
+++ src/component/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer_section() {
+  return (
+    <div className="w-full bg-amber-800 flex  ">
+      <div>
+        <div></div>
+        <div>
+          <h3 className="text-left text-white">
+            © 2025 Our first project, All Rights Reserved
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer_section;
