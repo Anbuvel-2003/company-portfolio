a4008691df2f5c43f2a3d5f9e4591469374df85c
Anbuvel-2003 <anbusmartanbuvel2407@gmail.com>
2025-07-12 17:56:36 +0530

    testing complted

--- src/pages/services/testing_Page/index.jsx
+++ src/pages/services/testing_Page/index.jsx
@@ -1,336 +1,169 @@
-import React, { useState } from "react";
-import Meetourclient from "../../../component/meetourclient";
-import software1 from "../../../assets/test1.jpg";
-import software2 from "../../../assets/test2.jpg";
-import software3 from "../../../assets/test3.jpg";
-import software4 from "../../../assets/test4.jpg";
-import { Flex, Progress, Card } from "antd";
+import testingbanner from "../../../assets/testingbanner.png";
+import testing1 from "../../../assets/testing1.png";
+import testing2 from "../../../assets/testing2.png";
+import testing3 from "../../../assets/testing3.png";
+import testing4 from "../../../assets/testing4.png";
+import service1 from "../../../assets/service1.png";
+import service2 from "../../../assets/service2.png";
+import service3 from "../../../assets/service3.png";
 
 function Testing_Page() {
-  const conicColors = {
-    "0%": "#b9e6fe",
-    "50%": "#ccdfff",
-    "100%": "#e7d5ff",
-  };
-  const data = [
+  const testing = [
     {
       id: 1,
-      title: "Manual",
-      paragraph:
-        "We execute real-world test scenarios to uncover bugs and UX issues before users do.",
-      points: [
-        {
-          id: 1,
-          data: "Functional User Testing.",
-        },
-        {
-          id: 2,
-          data: "UI Consistency Checks.",
-        },
-        {
-          id: 3,
-          data: "Regression Testing.",
-        },
-        {
-          id: 4,
-          data: "Exploratory Sessions.",
-        },
-      ],
+      image: testing1,
+      q1: "Test Automation",
+      q2: "Enhance your testing scope while reducing time and cost through automation.",
     },
     {
       id: 2,
-      title: "Automated",
-      paragraph:
-        "We create test scripts that accelerate QA and support continuous integration workflows.",
-      points: [
-        {
-          id: 1,
-          data: "Selenium Test Scripts.",
-        },
-        {
-          id: 2,
-          data: "Unit Test Coverage.",
-        },
-        {
-          id: 3,
-          data: "CI/CD Pipeline Testing.",
-        },
-        {
-          id: 4,
-          data: "Load Simulation Tools.",
-        },
-      ],
+      image: testing2,
+      q1: "Functional Testing",
+      q2: "Ensure your product development aligns seamlessly with comprehensive functional testing.",
     },
     {
       id: 3,
-      title: "Performance",
-      paragraph:
-        "Your app is tested under stress and against threats to ensure stability and safety.",
-      points: [
-        {
-          id: 1,
-          data: "Penetration Testing.",
-        },
-        {
-          id: 2,
-          data: "Stress & Load Testing.",
-        },
-        {
-          id: 3,
-          data: "SSL & Token Checks.",
-        },
-        {
-          id: 4,
-          data: "Vulnerability Scanning.",
-        },
-      ],
+      image: testing3,
+      q1: "Performance Testing",
+      q2: "Accurate guidelines to ensure high availability, performance, and speed of your application.",
+    },
+    {
+      id: 4,
+      image: testing4,
+      q1: "Security Testing",
+      q2: "Fortify your digital defenses with expert security testing.",
     },
   ];
-  const [selecteditem, setselecteditem] = useState(data[0]);
-  const { Meta } = Card;
+  const service = [
+    {
+      id: 1,
+      image: service1,
+      q1: "End-to-end testing expertise",
+      q2: "We provide end-to-end testing expertise, from Agile to post- development.",
+    },
+    {
+      id: 2,
+      image: service2,
+      q1: "A second pair of hands",
+      q2: "Extend your team's capabilities with Testvox's collaborative support.",
+    },
+    {
+      id: 3,
+      image: service3,
+      q1: "Expert opinion at your service",
+      q2: "Leverage Testvox's deep industry knowledge for insightful testing strategies.",
+    },
+  ];
   return (
-    <div className="!pt-15">
-      <div className="place-self-center lg:!p-20 md:!p-20 !p-5">
-        <h3 className="text-[#9e9e9e] font-syne lg:text-6xl sm:text-5xl " data-aos="zoom-in" >
-          Testing Services
-        </h3>
-      </div>
-      <div className="lg:flex lg:gap-0 ">
-        <div className="lg:w-1/2 w-full lg:!mb-0  !mb-10" data-aos="zoom-in" data-aos-duration="1000">
-          <img src={software1} alt="software1" className="" />
+    <section className="">
+      <div className="!mt-15">
+        <div className="bg-[#F8F4FF] md:!py-20 lg:!py-20 !py-10  ">
+          <h3 className="text-[#9e9e9e] font-syne lg:text-6xl sm:text-5xl text-center ">
+            Testing services
+          </h3>
         </div>
-        <div className="lg:w-1/2  lg:place-self-center !justify-items-center  md:!pl-40 sm:!pl-40 !pl-5 lg:!mb-0 !mb-5" data-aos="zoom-in" data-aos-duration="1000">
-          <div className="">
-            <h3 className="text-[#b3938a] text-[16px] tracking-wide uppercase ">
-              Who We Are
+        <div className="lg:!m-20 md:!m-20 md:flex lg:flex grid  sm:!m-10 !m-10">
+          <div className="lg:w-1/2 md:w-1/2 w-full  place-items-center lg:h-[60vh] md:h-[60vh]  ">
+            <img
+              alt=""
+              src={testingbanner}
+              className="w-[100%] h-[100%] object-contain"
+            />
+          </div>
+          <div className="lg:w-1/2 md:w-1/2 w-full place-content-center !mt-10 lg:!mt-0 md:!mt-0 ">
+            <h3 className="text-[20px] text-base/10 ">
+              Our UI/UX design services are crafted to elevate your digital
+              presence with precision and creativity. We begin by understanding
+              your goals and your audience, ensuring that every design decision
+              aligns with your brand’s vision. Our approach integrates user
+              research, wireframing, and prototyping to create intuitive and
+              engaging interfaces.Our UI/UX design services are crafted to
+              elevate your digital presence with precision and creativity. We
+              begin by understanding your goals and your audience, ensuring
             </h3>
-            <h3 className="text-black lg:text-5xl md:text-4xl text-3xl tracking-wide font-syne ">
-              Trusted <br />
-              and Supported <br /> by Many
-            </h3>
-            <div className="lg:flex grid md:flex !gap-3 !mt-3  lg:!gap-10 md:!gap-10 border-b-1 border-[#57b8e0] lg:w-[100%] md:w-[70%] w-[50%] !mb-4">
-              {data?.map((item, index) => {
-                const isselected = item?.id == selecteditem?.id;
-                return (
+          </div>
+        </div>
+        <div>
+          <h3 className="lg:text-[35px] md:text-[30px] sm:text-[25px] text-[20px] font-syne text-[#1A1A2E] text-center !my-10">
+            Technologies used
+          </h3>
+          <div className="bg-[#F8F4FF] !py-10 !px-5"></div>
+        </div>
+      </div>
+      <div className="lg:!m-20 md:!m-20 sm:!m-10 !m-10">
+        <div>
+          <h3 className="lg:text-[35px] md:text-[30px] sm:text-[25px] text-[20px] font-syne text-[#1A1A2E] text-center !my-5">
+            Enhance Your Software Quality with{" "}
+            <span className="text-[#AD46FF]">Vineatz Technologies</span>
+          </h3>
+          <h3 className="lg:text-[23px] md:text-[20px] sm:text-[18px] text-[16px] font-syne text-[#788094] text-center">
+            We helps you to acelerate your Product Releases with Our Expert
+            Testing team and ensuring optimal quality for your apps.
+          </h3>
+        </div>
+        <div className="lg:flex grid !mt-5">
+          <div className="lg:w-1/2 w-full grid lg:grid-cols-2 grid-cols-1 !gap-5 lg:!m-10 lg:mb-0 !mb-10">
+            {testing?.map((item, index) => {
+              const extraMarginTop = index === 1 || index === 3 ? "40px" : "0";
+              return (
+                <div style={{ marginTop: extraMarginTop }}>
                   <div
                     key={index}
-                    className={`lg:!py-3 md:!py-3 pr-10  ${isselected ? "border-b-1" : "border-0"
-                      } border-[#000000]`}
-                    onClick={() => {
-                      setselecteditem(item);
-                    }}
+                    className="!p-10 rounded-[10px] shadow-2xl !mt-5 "
                   >
-                    <p className="text-black lg:text-3xl md:text-3xl text-2xl tracking-wide font-syne cursor-pointer">
-                      {item?.title}
-                    </p>
+                    <img src={item?.image} alt="" className="" />
+                    <div>
+                      <h2 className="text-black text-[20px] capitalize font-syne !my-5 ">
+                        {item?.q1}
+                      </h2>
+                      <h2 className="text-[#555353] text-[18px] font-syne">
+                        {item?.q2}
+                      </h2>
+                    </div>
                   </div>
-                );
-              })}
-            </div>
-            <div className="">
-              <p className="lg:text-[20px] md:text-[18px] text-[16px] text-black !mb-4 font-sans lg:w-[90%] md:w-[70%] w-[60vw]">
-                {selecteditem?.paragraph}
-              </p>
-              <ul>
-                {selecteditem?.points &&
-                  selecteditem.points?.map((item, index) => {
-                    return (
-                      <li
-                        className="list-inside list-disc lg:text-[18px] md:text-[16px] text-[14px] font-sans"
-                        key={index}
-                      >
-                        {item?.data}
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="lg:w-1/2 w-full place-items-center place-content-center">
+            <img src={testingbanner} alt="" className=" object-contain " />
           </div>
         </div>
       </div>
-      <div className="lg:flex">
-        <div className="lg:w-1/2 w-full place-self-center justify-items-center lg:!mb-0 !mb-10 " data-aos="fade-right">
-          <div className="w-[60%]">
-            <h3 className="text-[#b3938a] text-[16px] tracking-wide uppercase !mb-3">
-              Brands
-            </h3>
-            <h2 className="text-black lg:text-5xl md:text-4xl text-3xl font-medium -tracking-tighter font-syne ">
-              Trusted <br /> and Supported <br /> by Many
-            </h2>
-            <div>
-              <div className="!my-5 ">
-                <h3 className="text-black lg:text-3xl md:text-3xl text-2xl  font-medium -tracking-tighter font-syne ">
-                  Manual
-                </h3>
-                <Progress
-                  percent={95}
-                  status="active"
-                  strokeColor={conicColors}
-                />
-              </div>
-              <div className="!my-5 ">
-                <h3 className="text-black lg:text-3xl md:text-3xl text-2xl font-medium -tracking-tighter font-syne ">
-                  Automated
-                </h3>
-                <Progress
-                  percent={75}
-                  status="active"
-                  strokeColor={conicColors}
-                />
-              </div>
-              <div className="!my-5 ">
-                <h3 className="text-black lg:text-3xl md:text-3xl text-2xl font-medium -tracking-tighter font-syne ">
-                  Performance
-                </h3>
-                <Progress
-                  percent={83}
-                  status="active"
-                  strokeColor={conicColors}
-                />
-              </div>
-            </div>
-          </div>
+      <div className="lg:!m-20 md:!m-20 sm:!m-10 !m-10 ">
+        <div>
+          <h3 className="lg:text-[35px] md:text-[30px] sm:text-[25px] text-[20px] font-syne text-[#1A1A2E] text-center !my-5">
+            Our Ways of Partnering
+          </h3>
         </div>
-        <div className="lg:w-1/2 w-full" data-aos="fade-left">
-          <img src={software2} alt="software1" className="" />
-        </div>
-      </div>
-      <div className="lg:flex" >
-        <div className="lg:w-1/2 w-full lg:mb-0 " data-aos="fade-right">
-          <img src={software3} alt="software1" className="" />
-        </div>
-        <div className="lg:w-1/2 w-full place-self-center justify-items-center lg:mb-0 !mb-10 " data-aos="fade-left">
-          <div className="w-[60%]">
-            <h3 className="text-[#b3938a] text-[16px] tracking-wide uppercase">
-              Built for Your Business
-            </h3>
-            <h2 className=" text-black lg:text-5xl md:text-4xl text-3xl  font-medium -tracking-tighter font-syne ">
-              Quality <br /> Assurance <br /> Process
-            </h2>
-            <div className="">
-              <p className="text-black text-[16px] font-normal">
-                Our testing workflow covers the entire SDLC—from requirement review to final sign-off. We use agile test cycles, issue tracking tools, and continuous feedback to ensure every release meets business standards and user expectations.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="lg:flex">
-        <div className="lg:w-1/2 w-full place-self-center justify-items-center lg:mb-0 !mb-10 " data-aos="fade-right">
-          <div className="w-[60%]">
-            <h3 className="text-[#b3938a] text-[16px] tracking-wide uppercase">
-              Built for Your Business
-            </h3>
-            <h2 className=" text-black lg:text-5xl md:text-4xl text-3xl  font-medium -tracking-tighter font-syne ">
-              Reliable <br /> Product <br /> Delivery
-            </h2>
-            <div className="">
-              <p className="text-black text-[16px] font-normal">
-                We make your software dependable through rigorous validation. Every module is tested for usability, stability, and resilience—minimizing post-launch issues and enhancing user trust.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/2 w-full " data-aos="fade-left">
-          <img src={software4} alt="software1" className="" />
-        </div>
-      </div>
-      {/* <div className="flex !my-20">
-        <div className="w-full h-[50%] relative">
-          <img
-            src={
-              "https://deon.qodeinteractive.com/wp-content/uploads/2021/11/Home-6-parallax-img.jpg"
-            }
-            alt="software2"
-            className=""
-          />
-          <div className="bg-white absolute top-0 ">
-            <h3 className="text-[#b3938a] text-[16px] tracking-wide uppercase !mb-3">
-              Brands
-            </h3>
-            <h2 className="text-black text-5xl font-medium -tracking-tighter font-syne ">
-              Trusted <br /> and Supported <br /> by Many
-            </h2>
-          </div>
-        </div>
-      </div> */}
-      <div className="relative w-full h-[800px]" data-aos="fade-up" >
-        <img
-          src="https://deon.qodeinteractive.com/wp-content/uploads/2021/11/Home-6-parallax-img.jpg"
-          alt="software2"
-          className="absolute inset-0 w-full h-full object-cover"
-        />
-
-        {/* Overlay content */}
-        <div className="absolute inset-0 flex items-center justify-center" data-aos="fade-up" data-aos-duration="3000">
-          <div className="bg-transparent bg-opacity-80 !p-8 max-w-xl text-left">
-            <h3 className="text-white text-[16px] tracking-wide uppercase mb-3">
-              Brands
-            </h3>
-            <h2 className="text-white lg:text-5xl md:text-4xl text-3xl font-medium font-syne leading-tight">
-              Trusted and <br /> Supported <br />
-            </h2>
-            <p className="text-white lg:text-[16px] md:text-[14px] text-[12px] font-normal">
-              Custom software development is the process of building tailored
-              software solutions designed specifically to meet the unique needs
-              of a business or organization. Unlike off-the-shelf software,
-              custom solutions are developed from the ground up to align with
-              specific workflows, goals, and challenges, offering greater
-              flexibility, scalability, and efficiency.
-            </p>
-            <div className="!mt-5">
-              <div className="!px-5 !py-2 border-1 border-white w-fit group hover:bg-white">
-                <span className="text-white font-syne text-[16px] group-hover:text-transparent group-hover:bg-gradient-to-r group-hover:from-indigo-500 group-hover:to-cyan-400 group-hover:bg-clip-text">
-                  View More
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* <div className="!my-20">
-        <h3 className=" text-black text-5xl font-medium -tracking-tighter font-syne text-center ">
-          Our Services
-        </h3>
-        <div className="!my-20">
-          <div className="flex flex-wrap justify-center items-center lg:!px-80 md:!px-10">
+        <div className="!mt-20">
+          <div className="grid lg:grid-cols-3 grid-cols-1 lg:!gap-5 ">
             {service?.map((item, index) => {
               return (
-                <div
-                  key={index}
-                  className="lg:w-1/3 md:w-1/3 sm:w-1/2 w-full flex justify-center"
-                >
-                  <div className="!mb-5">
-                    <Card
-                      hoverable
-                      style={{ width: "80%" }}
-                      cover={<img alt="example" src={item?.img} />}
-                    >
-                      <Meta
-                        title={item?.title}
-                        description={item?.description}
-                        className="font-syne"
-                      />
-                      <div className="">
-                        <div className="!p-2 place-self-end">
-                          <a href={item?.path} className="">
-                            <span className="text-[16px] capitalize">
-                              Read more{" "}
-                            </span>
-                          </a>
-                        </div>
-                      </div>
-                    </Card>
+                <div className="!mb-[70px]">
+                  <div
+                    key={index}
+                    className="rounded-[10px] shadow-2xl lg:!p-20 md:!p-15 !p-10 relative"
+                  >
+                    <div className="absolute lg:top-[-20%] lg:left-[40%] md:left-[45%] left-[30%] top-[-30%]">
+                      <img src={item?.image} className="" alt="" />
+                    </div>
+                    <div className="place-items-center">
+                      <h2 className="text-[20px] text-black font-syne capitalize !mb-3 text-center">
+                        {item?.q1}
+                      </h2>
+                      <h2 className="text-[16px] text-[#555353] font-syne text-center">
+                        {item?.q2}
+                      </h2>
+                    </div>
                   </div>
                 </div>
               );
             })}
           </div>
         </div>
-      </div> */}
-      <Meetourclient />
-    </div>
+      </div>
+    </section>
   );
 }
 
